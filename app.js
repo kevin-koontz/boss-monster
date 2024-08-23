a648749749a18418ded5139468189c3f615ebff8
Kevin Koontz <contactkkoontz@gmail.com>
2024-08-23 08:29:00 -0700

    layout pass 1, will complete functionality after trip

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log(`hello`);
+
+const heroes = [
+  {
+    name: 'Slate Slabrock',
+    type: 'dwarf',
+    damage: 5,
+    health: 100
+  },
+  {
+    name: 'Swift Ironstag',
+    type: 'elf',
+    damage: 10,
+    health: 50
+  }
+]
+
+const boss = {
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1
+}
